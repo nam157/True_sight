d27ad5b834069763310d86463e858bf819d2753b
dark157 <nguynhoainam15@gmail.com>
2022-03-08 22:31:23 +0700

    add ssd-resnet

---
+++ object_detection/SSD-Resnet50/data_loader/dataset.py
@@ -0,0 +1,54 @@
+from matplotlib.pyplot import annotate
+from numpy import size
+import torch
+from torchvision.datasets import CocoDetection
+from torch.utils.data.dataloader import default_collate
+import os
+
+"""
+Chúng ta sử dụng framework pytorch, trong torchvision.datasets có chứa dataset COCO
+"""
+def collate_fn(batch):
+    items = list(zip(*batch))
+    items[0] = default_collate([i for i in items[0] if torch.is_tensor(i)])
+    items[1] = list([i for i in items[1] if i])
+    items[2] = list([i for i in items[2] if i])
+    items[3] = default_collate([i for i in items[3] if torch.is_tensor(i)])
+    items[4] = default_collate([i for i in items[4] if torch.is_tensor(i)])
+    return items
+
+class DatasetCOCO(CocoDetection):
+    def __init__(self,root_path,year,mode,transform = None):
+        annFile = os.path.join(root_path,'annatations','instances_{}{}.json'.format(mode,year))
+        root = os.path.join(root_path,'{}{}'.format(mode,year))
+        super(DatasetCOCO,self).__init__(root,annFile)
+        self._load_categories()
+        self.transform = transform
+
+    def _load_categories(self):
+        categories = self.coco.loadCats(self.coco.getCatIds())
+        categories.sort(key = lambda x:x['id'])
+
+        self.label_map = {}
+        self.label_info = {}
+        counter = 1
+        self.label_info[0] = 'background'
+        for c in categories:
+            self.label_map[c['id']] = counter
+            self.label_info[counter] = c['name']
+            counter +=1
+    def __getitem__(self, item):
+        image, target = super(DatasetCOCO, self).__getitem__(item)
+        width, height = image.size
+        boxes,labels = [],[]
+        if len(target) == 0:
+            return None,None,None,None,None
+        for annotation in target:
+            bbox = annotation.get('bbox')
+            boxes.append([bbox[0] / width, bbox[1] / height, (bbox[0] + bbox[2]) / width, (bbox[1] + bbox[3]) / height])
+            labels.append(self.label_map[annotation.get("category_id")])
+        boxes = torch.tensor(boxes)
+        labels = torch.tensor(labels)
+        if self.transform is not None:
+            image, (height, width), boxes, labels = self.transform(image, (height, width), boxes, labels)
+        return image, target[0]["image_id"], (height, width), boxes, labels
