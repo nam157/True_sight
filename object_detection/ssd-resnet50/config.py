c59fae2b3a7ba4015eddfeb7ab9ad6a2f7c5b973
Nguyễn Hoài Nam <nguyenhoainamhuongkhe2@gmail.com>
2022-03-19 15:02:18 +0700

    Update config.py

--- object_detection/ssd-resnet50/config.py
+++ object_detection/ssd-resnet50/config.py
@@ -9,6 +9,7 @@
 #                  "lò vi sóng", "lò nướng", "máy nướng bánh mì", "bồn rửa", "tủ lạnh", "sách", "đồng hồ", "bình hoa", "kéo",
 #                  "gấu bông", "máy sấy tóc", "bàn chải đánh răng"]
 
+
 coco_classes = ["background", "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
                 "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog",
                 "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
@@ -39,8 +40,31 @@ colors = [None, (39, 129, 113), (164, 80, 133), (83, 122, 114), (99, 81, 172), (
           (2, 20, 184), (122, 37, 185)]
 
 input = 'C:/Users/nguye/OneDrive/Desktop/ai4theblind/VGG_SSD/data/test_img/aa.jpg'
-pretrained_model = 'C:/Users/nguye/OneDrive/Desktop/ai4theblind/SSD_1/weights/SSD.pth'
+pretrained_model = 'C:/Users/nguye/OneDrive/Desktop/ai4theblind/Resnet-ssd/weights/ssd300_68.pth'
 nms_threshold = 0.4
 cls_threshold  = 0.4
 pTime = 0
 cTime = 0
+
+CENTERS = {'background': {}, 'person': {}, 'bicycle': {}, 'car': {}, 'motorcycle': {}, 'airplane': {}, 'bus': {}, 
+            'train': {}, 'truck': {}, 'boat': {}, 'traffic light': {}, 'fire hydrant': {}, 'stop sign': {}, 'parking meter': {}, 
+            'bench': {}, 'bird': {}, 'cat': {}, 'dog': {}, 'horse': {}, 'sheep': {}, 'cow': {}, 'elephant': {}, 'bear': {}, 'zebra': {}, 
+            'giraffe': {}, 'backpack': {}, 'umbrella': {}, 'handbag': {}, 'tie': {}, 'suitcase': {}, 'frisbee': {}, 'skis': {}, 
+            'snowboard': {}, 'sports ball': {}, 'kite': {}, 'baseball bat': {}, 'baseball glove': {}, 'skateboard': {}, 'surfboard': {},
+            'tennis racket': {}, 'bottle': {}, 'wine glass': {}, 'cup': {}, 'fork': {}, 'knife': {}, 'spoon': {}, 
+            'bowl': {}, 'banana': {}, 'apple': {}, 'sandwich': {}, 'orange': {}, 'broccoli': {}, 'carrot': {}, 
+            'hot dog': {}, 'pizza': {}, 'donut': {}, 'cake': {}, 'chair': {}, 'couch': {}, 'potted plant': {}, 
+            'bed': {}, 'dining table': {}, 'toilet': {}, 'tv': {}, 'laptop': {}, 'mouse': {}, 'remote': {}, 'keyboard': {}, 
+            'cell phone': {}, 'microwave': {}, 'oven': {}, 'toaster': {}, 'sink': {}, 'refrigerator': {}, 'book': {},
+            'clock': {}, 'vase': {}, 'scissors': {}, 'teddy bear': {}, 'hair drier': {}, 'toothbrush': {}}
+boxes = {'background': {}, 'person': {}, 'bicycle': {}, 'car': {}, 'motorcycle': {}, 'airplane': {}, 'bus': {}, 
+            'train': {}, 'truck': {}, 'boat': {}, 'traffic light': {}, 'fire hydrant': {}, 'stop sign': {}, 'parking meter': {}, 
+            'bench': {}, 'bird': {}, 'cat': {}, 'dog': {}, 'horse': {}, 'sheep': {}, 'cow': {}, 'elephant': {}, 'bear': {}, 'zebra': {}, 
+            'giraffe': {}, 'backpack': {}, 'umbrella': {}, 'handbag': {}, 'tie': {}, 'suitcase': {}, 'frisbee': {}, 'skis': {}, 
+            'snowboard': {}, 'sports ball': {}, 'kite': {}, 'baseball bat': {}, 'baseball glove': {}, 'skateboard': {}, 'surfboard': {},
+            'tennis racket': {}, 'bottle': {}, 'wine glass': {}, 'cup': {}, 'fork': {}, 'knife': {}, 'spoon': {}, 
+            'bowl': {}, 'banana': {}, 'apple': {}, 'sandwich': {}, 'orange': {}, 'broccoli': {}, 'carrot': {}, 
+            'hot dog': {}, 'pizza': {}, 'donut': {}, 'cake': {}, 'chair': {}, 'couch': {}, 'potted plant': {}, 
+            'bed': {}, 'dining table': {}, 'toilet': {}, 'tv': {}, 'laptop': {}, 'mouse': {}, 'remote': {}, 'keyboard': {}, 
+            'cell phone': {}, 'microwave': {}, 'oven': {}, 'toaster': {}, 'sink': {}, 'refrigerator': {}, 'book': {},
+            'clock': {}, 'vase': {}, 'scissors': {}, 'teddy bear': {}, 'hair drier': {}, 'toothbrush': {}}
